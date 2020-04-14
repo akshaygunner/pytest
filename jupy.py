f13b98c55567d176b3c72017bad833dd47f7f01c
EC2 Default User <ec2-user@ip-172-16-4-105.ec2.internal>
2020-04-14 19:34:56 +0000

    new files

---
+++ jupy.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python [conda env:python3]
+#     language: python
+#     name: conda-env-python3-py
+# ---
+
+import pandas as pd
+
+lst = [1,2,3]
+
+df = pd.DataFrame(lst)
+
+df
+
+cnt = df.size
+
+cnt
+
+
